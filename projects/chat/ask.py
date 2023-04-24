7433231f0952fb135a0c8591e3677fe75cc97cd1
andysingal <andysingal@gmail.com>
2023-04-24 17:10:46 +0530

    chat-qa- 1

---
+++ projects/chat/ask.py
@@ -0,0 +1,29 @@
+from dotenv import dotenv_values, load_dotenv
+load_dotenv()
+config = dotenv_values("/Users/ankushsingal/Desktop/PycharmProjects/langchain/projects/.env")
+
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.vectorstores import DeepLake
+
+org = config["ACTIVELOOP_ORG"]
+
+dataset_path = 'hub://'+org+'/data'
+#
+embeddings = OpenAIEmbeddings()
+
+db = DeepLake(dataset_path=dataset_path, read_only=True, embedding_function=embeddings)
+
+retriever = db.as_retriever()
+retriever.search_kwargs['distance_metric'] = 'cos'
+retriever.search_kwargs['k'] = 4
+
+qa = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=retriever, return_source_documents=False)
+
+query = input("Enter query:")
+
+ans = qa({"query": query})
+
+print(ans)
