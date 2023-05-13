42dc022414517d70e2c629a47aeda12248a6d7a0
andysingal <andysingal@gmail.com>
2023-05-13 19:23:27 +0530

    3-1

--- icebreaker/ice_breaker.py
+++ icebreaker/ice_breaker.py
@@ -5,16 +5,15 @@ from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from output_parsers import person_intel_parser, PersonIntel
 
-name = "Harrison Chase"
-if __name__ == "__main__":
-    print("Hello LangChain")
 
+def ice_break(name: str) -> str:
     linkedin_profile_url = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
     twitter_username = twitter_lookup_agent(name=name)
-    tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
+    tweets = scrape_user_tweets(username=twitter_username, num_tweets=1)
 
     summary_template = """
           given the Linkedin information {linkedin_information} and twitter {twitter_information} about a person from what i want you to create:
@@ -22,13 +21,25 @@ if __name__ == "__main__":
           2. two interesting facts about them
           3. A topic that may interest them 
           4. 2 creative Ice breakers to open a conversation with them 
+             \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
         input_variables=["linkedin_information", "twitter_information"],
         template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+    result = chain.run(linkedin_information=linkedin_data, twitter_information=tweets)
+    print(result)
+    return person_intel_parser.parse(result)
+
+
+name = "Harrison Chase"
+if __name__ == "__main__":
+    print("Hello LangChain")
+    result = ice_break(name="Harrison Chase")
